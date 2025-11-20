a1e2d1e1bbaedbf7fbd428298bc0961ef1eb1001
alissonmoreira7 <alonmoreira.dev@gmail.com>
2025-11-20 14:35:27 -0300

    Realizando teste para saber se a localização da coleta de dados no arquivo JSON está correta

--- etl_clima.py
+++ etl_clima.py
@@ -6,8 +6,16 @@ if __name__ == "__main__":
     dados_brutos = extrair_dados_previsao(CIDADE_TESTE, API_KEY, BASE_URL)
 
     if dados_brutos is not None:
-        print("\n✅ Extração bem-sucedida! Pronto para a Transformação.")
+        print("\nExtração bem-sucedida! Pronto para a Transformação.")
         print(f"Chaves do JSON: {dados_brutos.keys()}")
+       
     else:
-        print("❌ A extração falhou. Verifique as credenciais ou a conexão.")
+        print("A extração falhou. Verifique as credenciais ou a conexão.")
+
+    for item in dados_brutos['list']:
+        data_previsao = item['dt_txt']
+        temperatura = item['main']['temp']
+
+    print(data_previsao, temperatura)
+
 
