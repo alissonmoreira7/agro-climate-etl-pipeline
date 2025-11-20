ad847991107ff83350bb94ddffdee2a32b1238c8
alissonmoreira7 <alonmoreira.dev@gmail.com>
2025-11-20 14:07:28 -0300

    Realizando conexão com a API para conseguir dados em JSON

---
+++ src/functions.py
@@ -0,0 +1,18 @@
+import requests
+
+def extrair_dados_previsao(cidade, api_key, base_url):
+    parametros = {
+        "q": cidade,
+        "appid": api_key,
+        "units": "metric"
+    }
+
+    response = requests.get(base_url, params=parametros)
+
+    if response.status_code == 200:
+        print("Conexão bem-sucedida! Dados brutos bem recebidos.")
+        return response.json()
+
+    else:
+        print(f"Erro na conexão! Status Code: {response.status_code}")
+        return None
