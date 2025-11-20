57a04c8d9419b3522fa0e62a8309dc8e3b4df4c4
AlonMoreira <alissonlim13@gmail.com>
2025-11-20 16:01:22 -0300

    Tratamento de dados, transformando os dados em JSON para uma lista.

--- etl_clima.py
+++ etl_clima.py
@@ -6,16 +6,25 @@ if __name__ == "__main__":
     dados_brutos = extrair_dados_previsao(CIDADE_TESTE, API_KEY, BASE_URL)
 
     if dados_brutos is not None:
-        print("\nExtração bem-sucedida! Pronto para a Transformação.")
         print(f"Chaves do JSON: {dados_brutos.keys()}")
        
     else:
         print("A extração falhou. Verifique as credenciais ou a conexão.")
 
-    for item in dados_brutos['list']:
-        data_previsao = item['dt_txt']
-        temperatura = item['main']['temp']
+    dados_tratados = []
 
-    print(data_previsao, temperatura)
+    for item in dados_brutos['list']:
+        registro = {
+            'cidade': dados_brutos['city']['name'],
+            'data_previsao': item['dt_txt'],
+            'temperatura': item['main']['temp'],
+            'umidade': item['main']['humidity'],
+            'proba_chuva': round(item.get('pop', 0)*100, 2)
+        }
+
+        dados_tratados.append(registro)
+        
+
+   
 
 
