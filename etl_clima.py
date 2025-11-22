4a5994addc852040454c79cc54d732652dc72346
Alisson Moreira <alonmoreira.dev@gmail.com>
2025-11-21 21:16:28 -0300

    Adicionando a coleta dos dados de cada cidade do Matopiba - OBS: Erro em Luis Eduardo Magalhaes

--- etl_clima.py
+++ etl_clima.py
@@ -1,28 +1,47 @@
+#coletar latitude e longitude
+import pandas as pd
 from src.functions import extrair_dados_previsao
-from config.settings import CIDADE_TESTE, API_KEY, BASE_URL
+from config.settings import API_KEY, BASE_URL
+
+LISTA_CIDADES = [
+    'Luis Eduardo Magalhaes,BR', 
+    'Barreiras,BR',              
+    'Balsas,BR',                 
+    'Gurupi,BR'                   
+]
 
 if __name__ == "__main__":
 
-    dados_brutos = extrair_dados_previsao(CIDADE_TESTE, API_KEY, BASE_URL)
-
-    if dados_brutos is not None:
-        print(f"Chaves do JSON: {dados_brutos.keys()}")
-       
-    else:
-        print("A extração falhou. Verifique as credenciais ou a conexão.")
-
     dados_tratados = []
 
-    for item in dados_brutos['list']:
-        registro = {
-            'cidade': dados_brutos['city']['name'],
-            'data_previsao': item['dt_txt'],
-            'temperatura': item['main']['temp'],
-            'umidade': item['main']['humidity'],
-            'proba_chuva': round(item.get('pop', 0)*100, 2)
-        }
+    for cidade in LISTA_CIDADES:
+        print(f"Buscando dados para: {cidade}")
+        
+        
+        dados_brutos = extrair_dados_previsao(cidade, API_KEY, BASE_URL)
+        
+        if dados_brutos is None:
+            print(f"Falha na extração para {cidade}. Pulando...")
+            continue
 
-        dados_tratados.append(registro)
+
+    for item in dados_brutos['list']:
+            registro = {
+                'cidade': dados_brutos['city']['name'],
+                'data_previsao': item['dt_txt'],
+                'temperatura': item['main']['temp'],
+                'umidade': item['main']['humidity'],
+                'proba_chuva': round(item.get('pop', 0) * 100, 2),
+                'latitude': dados_brutos['city']['coord']['lat'],
+                'longitude': dados_brutos['city']['coord']['lon']
+            }
+            dados_tratados.append(registro)
+
+    clima_df = pd.DataFrame(dados_tratados) 
+    
+    print(clima_df.head())
+        
+   
         
 
    
