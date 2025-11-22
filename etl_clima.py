d8991447ba8302da29c90b8f60d84b856cd92ead
alissonmoreira7 <alonmoreira.dev@gmail.com>
2025-11-21 23:44:44 -0300

    Conexão com o banco de dados efetuada! Envio dos dados concluído!

--- etl_clima.py
+++ etl_clima.py
@@ -1,7 +1,7 @@
-#coletar latitude e longitude
 import pandas as pd
-from src.functions import extrair_dados_previsao
-from config.settings import API_KEY, BASE_URL
+from src.functions import extrair_dados_previsao, carregar_dados
+from config.settings import API_KEY, BASE_URL, DB_CONNECTION_STRING
+
 
 LISTA_CIDADES = [
     'Luis Eduardo Magalhaes,BR', 
@@ -17,33 +17,26 @@ if __name__ == "__main__":
     for cidade in LISTA_CIDADES:
         print(f"Buscando dados para: {cidade}")
         
-        
         dados_brutos = extrair_dados_previsao(cidade, API_KEY, BASE_URL)
         
         if dados_brutos is None:
             print(f"Falha na extração para {cidade}. Pulando...")
             continue
 
-
     for item in dados_brutos['list']:
-            registro = {
-                'cidade': dados_brutos['city']['name'],
-                'data_previsao': item['dt_txt'],
-                'temperatura': item['main']['temp'],
-                'umidade': item['main']['humidity'],
-                'proba_chuva': round(item.get('pop', 0) * 100, 2),
-                'latitude': dados_brutos['city']['coord']['lat'],
-                'longitude': dados_brutos['city']['coord']['lon']
-            }
-            dados_tratados.append(registro)
+        registro = {
+            'cidade': dados_brutos['city']['name'],
+            'data_previsao': item['dt_txt'],
+            'temperatura': item['main']['temp'],
+            'umidade': item['main']['humidity'],
+            'proba_chuva': round(item.get('pop', 0) * 100, 2),
+            'latitude': dados_brutos['city']['coord']['lat'],
+            'longitude': dados_brutos['city']['coord']['lon']
+        }
+        dados_tratados.append(registro)
 
     clima_df = pd.DataFrame(dados_tratados) 
     
     print(clima_df.head())
         
-   
-        
-
-   
-
-
+    carregar_dados(DB_CONNECTION_STRING, clima_df)
