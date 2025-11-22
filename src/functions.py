7651faf7363365f80912d03c734f06bfe5030d45
AlonMoreira <alissonlim13@gmail.com>
2025-11-21 23:44:44 -0300

    Conexão com o banco de dados efetuada! Envio dos dados concluído!

--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,8 @@
 import requests
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
 
-def extrair_dados_previsao(cidade, api_key, base_url):
+def extrair_dados_previsao(cidade, api_key, base_url,):
     parametros = {
         "q": cidade,
         "appid": api_key,
@@ -12,8 +14,21 @@ def extrair_dados_previsao(cidade, api_key, base_url):
     if response.status_code == 200:
         print("Conexão bem-sucedida! Dados brutos bem recebidos.")
         return response.json()
-
     else:
         print(f"Erro na conexão! Status Code: {response.status_code}")
         return None
     
+def carregar_dados(conexao, df):
+
+    engine = create_engine(conexao) 
+    
+    
+    df.to_sql(
+        name='previsao_clima', 
+        con=engine,
+        if_exists='append', 
+        index=False,
+        chunksize=5000
+    )
+    print("Carga (L) no MySQL concluída com sucesso!")
+        
